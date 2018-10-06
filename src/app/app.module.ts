278f3b7e6ad4c1d34d8ec24710bd54c79912628d
Hishigee <hishigbayr_3@yahoo.com>
2018-10-06 22:20:46 +0800

    use Firebase lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,18 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
+//Firebase 
 import { FIREBASE_CONFIG } from '../app/firebase.config';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+//Firebase
+//Pages
 import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
 import { RegisterPage } from '../pages/register/register';
 import { UserProfilePage } from '../pages/user-profile/user-profile';
+//Pages
 
 @NgModule({
   declarations: [
@@ -21,6 +28,9 @@ import { UserProfilePage } from '../pages/user-profile/user-profile';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
