d808fc24cdc95560c8e16642db64ca636688be6e
Hishigee <hishigbayr_3@yahoo.com>
2018-10-06 22:50:11 +0800

    add profile model

---
+++ src/models/profile.ts
@@ -0,0 +1,11 @@
+export class Profile {
+    lastName: string;
+    firstName: string;
+    registerNumber: string;
+    email: string;
+    phoneNumber: string;
+    image: string;
+    age: string;
+    sex: string;
+    state: string;
+}
