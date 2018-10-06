6b419263f804abe267a28f70d73c000494ea3bcc
Hishigee <hishigbayr_3@yahoo.com>
2018-10-06 22:07:04 +0800

    Design and FireBase Config, root page soliw

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,19 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
+import { FIREBASE_CONFIG } from '../app/firebase.config';
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { RegisterPage } from '../pages/register/register';
+import { UserProfilePage } from '../pages/user-profile/user-profile';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    RegisterPage,
+    UserProfilePage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +26,10 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    RegisterPage,
+    UserProfilePage
   ],
   providers: [
     StatusBar,
---
+++ src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDrY6Xvj3iNojMcezydp3x5XXN6R5B7Z1U",
+    authDomain: "busticket-608bb.firebaseapp.com",
+    databaseURL: "https://busticket-608bb.firebaseio.com",
+    projectId: "busticket-608bb",
+    storageBucket: "busticket-608bb.appspot.com",
+    messagingSenderId: "397095982773"
+}
