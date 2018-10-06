3a2de1609ee1e61a41008d745605e7d598da4064
Hishigee <hishigbayr_3@yahoo.com>
2018-10-06 22:45:15 +0800

    home.ts add

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { LoginPage } from '../login/login';
+import { AuthenticationProvider } from '../../providers/authentication/authentication';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private authenticationProvider: AuthenticationProvider) {
   }
 
+  ionViewDidLoad() {
+    // body
+  } 
+
+  logout() {
+    this.authenticationProvider.logOut();
+    this.navCtrl.setRoot(LoginPage);
+  }
 }
