3f865f1a2b63aff5c21e0288629391efbe16ada9
Hishigee <hishigbayr_3@yahoo.com>
2018-10-07 17:06:24 +0800

    register validation

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { FormControl, FormGroup, FormBuilder, Validators} from '@angular/forms';
 import { User } from '../../models/user';
 import { AuthenticationProvider } from '../../providers/authentication/authentication';
 import { ProfileProvider } from '../../providers/profile/profile';
@@ -14,13 +14,40 @@ import { Profile } from '../../models/profile';
 export class RegisterPage {
   user = {} as User;
   profile = {} as Profile;
+  registerForm: FormGroup;
+  emailErrorHide = false;
+  passErrorHide = false;
+  
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     private authenticationProvider: AuthenticationProvider,
-    private profileProvider: ProfileProvider
-  ) { }
+    private profileProvider: ProfileProvider,
+    private fb: FormBuilder
+  ) {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
 
+  changeInputText(e) {
+    if(e.target.name == "email")
+      this.emailErrorHide = false;
+    if(e.target.name == "password")
+      this.passErrorHide = false;
+  }
+  get formFields() { return this.registerForm.controls; }
+
+  closeErrorMsg(e) {
+    if(e.target.parentNode.parentNode.parentNode.children[0].children[0].name == "email") { 
+      this.emailErrorHide = true; 
+    }
+    if(e.target.parentNode.parentNode.parentNode.children[0].children[0].name == "password") {
+      this.passErrorHide = true;
+    }
+      
+  }
   ionViewDidLoad() {
     // body
   }
@@ -35,7 +62,7 @@ export class RegisterPage {
       console.log("Amjilttai");
     },
     error=>{
-      console.log(error.message);
+      console.log(error);
     });
     this.navCtrl.pop();
   }
